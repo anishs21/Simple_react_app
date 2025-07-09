8c3eb820c8ab24c5e3ff3cc5cf43fd86b4955447
Anisha S <anishasivasubramani@gmail.com>
2025-07-09 11:53:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Welcome to Simple React Frontend!</h1>
+      <button onClick={() => alert('Hello, Anisha!')}>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
